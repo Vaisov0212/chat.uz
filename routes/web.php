f5f65e47bc3d3782f1d833a3ff6b3c7923e8670c
Vaisov0212 <karvak117@mail.ru>
2021-06-02 14:38:55 +0500

    create controller

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('index');
-});
+Route::get('/','SiteController@index');
 
 Route::namespace('Admin')->middleware('auth')->name('admin.')->prefix('l-admin')->group(function (){
 
